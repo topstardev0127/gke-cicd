34dcc63b01cd347f4373685f2b7d20deadcb9096
Ubuntu <ubuntu@ip-172-31-24-83.us-west-2.compute.internal>
2024-07-10 18:08:56 +0000

    update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    return 'Hey this is cicd test!'
+    return 'Hey this is cicd test again test Uhahahaha!'
 
 @app.route('/about')
 def about():
